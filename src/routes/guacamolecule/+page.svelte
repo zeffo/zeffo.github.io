c3a0c40f0b8e3bd15d1b6a3a5965fca6a4dbe8c3
Aman Phatak <phatakaman@gmail.com>
2022-09-12 23:40:11 +0530

    add guacamolecule invite redirect

---
+++ src/routes/guacamolecule/+page.svelte
@@ -0,0 +1,9 @@
+<head>
+    <meta property="og:title" content="Guacamolecule" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Aman's discord server" />
+    <meta name="theme-color" content="#f5c2e7">
+    <meta http-equiv="refresh" content="0; url=https://discord.gg/6frntWtKFj" />
+</head>
+
+
